a997ce72da65355c56d8150a393277a53d8fe657
AhmedAboAbadi <ahmed.m.aboabadi@gmail.com>
2022-10-30 15:13:41 +0200

    OS modul

---
+++ OS/index.js
@@ -0,0 +1,18 @@
+const os = require("os");
+
+// Path: app.js
+const user = os.userInfo();
+console.log(user);
+
+
+console.log(`The System Uptime is ${os.uptime()} seconds`);
+
+
+const currentOS = {
+  name : os.type(),
+  release : os.release(),
+  totalMem : os.totalmem(),
+  freeMem : os.freemem(),
+}
+
+console.log(currentOS);
