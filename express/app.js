dcc757c36b52149951bd3395e4f1a359cb210123
AhmedAboAbadi <ahmed.m.aboabadi@gmail.com>
2022-11-01 15:41:15 +0200

    create product endpoint ,pagination & adding search

--- express/app.js
+++ express/app.js
@@ -7,14 +7,45 @@ const { products } = require("./data");
 app.get("/", (req, res) => {
   res.send("<h1>Home Page</h1><a href='/api/products'>products</a>");
 });
+app.get("/api/products/", (req, res) => {
+  console.log("🚀 ~ file: app.js ~ line 11 ~ app.get ~ req", req)
+  res.json(products);
+});
 
-app.get("/api/products", (req, res) => {
-  const newProducts = products.map((product) => {
-    const { id, name, image } = product;
-    return { id, name, image };
-  });
+app.get("/api/products/:productId", (req, res) => {
+  const productId = req.params.productId;
+  const singleProduct = products.find(
+    (product) => product.id === parseFloat(productId)
+  );
+  if (!singleProduct) {
+    return res.status(404).send("Product does not exist");
+  }
+  return res.json(singleProduct);
+});
 
-  res.json(newProducts);
+app.get("/api/v1/query", (req, res) => {
+  const { search, limit } = req.query;
+  let sortedProducts = [...products];
+
+  if (search) {
+    sortedProducts = sortedProducts.filter((product) => {
+      return product.name.startsWith(search);
+    });
+  }
+  if (limit) {
+    sortedProducts = sortedProducts.slice(0, Number(limit));
+  }
+
+  if (sortedProducts.length < 1) {
+    // res.send(
+    //   '<h1>no products matched your search</h1><a href="/">back home</a>'
+    // );
+    res.status(200).json({ success: true, data: [] });
+  }
+
+  res.status(200).json(sortedProducts);
+
+  res.send("hello world");
 });
 
 app.get("/about", (req, res) => {
@@ -22,7 +53,6 @@ app.get("/about", (req, res) => {
 });
 
 app.all("*", (req, res) => {
-  console.log("404");
   res.status(404).send("OPS!!, page not found!!");
 });
 app.listen(5000, () => {
