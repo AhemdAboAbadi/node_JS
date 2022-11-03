71b46a2a44224ec2e2ed5bbe2e2fdaae20089f5f
AhmedAboAbadi <ahmed.m.aboabadi@gmail.com>
2022-11-03 15:10:15 +0200

    prisma

--- nest-js/src/main.ts
+++ nest-js/src/main.ts
@@ -1,5 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { PrismaClient } from '@prisma/client';
+const prisma = new PrismaClient();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
